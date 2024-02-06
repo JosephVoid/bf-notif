f564027b5f017af578279f317a130702b624dff1
Yoseph <yosephten@gmail.com>
2024-02-06 18:20:28 +0300

    `feat`: added email sending

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
 import os, sys, ast
 import pika
 import time
+import smtplib
 from dotenv import load_dotenv
 import requests
 from requests.auth import HTTPBasicAuth
+from email.mime.text import MIMEText
 
 load_dotenv()
 
-def log (output: str):
-    with open('log.txt', 'a+', encoding = 'UTF-8') as f:
-        f.writelines(time.ctime(time.time())+" : "+ f'{output}'+"\n")
 
-def send_sms (to: str, msg: str) -> bool:
-    payload = {"message": msg, "phoneNumbers":[to]}
+def log(output: str):
+    with open("log.txt", "a+", encoding="UTF-8") as f:
+        f.writelines(time.ctime(time.time()) + " : " + f"{output}" + "\n")
 
+
+def send_sms(to: str, msg: str) -> bool:
+    payload = {"message": msg, "phoneNumbers": [to]}
     try:
-        response = requests.post(os.environ['SMS_URL'], auth=HTTPBasicAuth(os.environ['SMS_USR'], os.environ['SMS_PSS']), data=payload)
-        log(str(response.status_code)+'-:-'+response.text)
-        if (response.status_code >= 200 or response.status_code < 210):
+        response = requests.post(
+            os.environ["SMS_URL"],
+            auth=HTTPBasicAuth(os.environ["SMS_USR"], os.environ["SMS_PSS"]),
+            data=payload,
+        )
+        log(str(response.status_code) + "-:-" + response.text)
+        if response.status_code >= 200 or response.status_code < 210:
             log(payload)
             return True
         else:
@@ -27,18 +34,35 @@ def send_sms (to: str, msg: str) -> bool:
         log(e)
         return False
 
-def send_email (to: str, body: str, subject: str):
-    pass
 
-def on_recieve (ch, method, props, body: bytes):
+def send_email(to: str, body: str, subject: str):
+    try:
+        email_msg = MIMEText(f"{body}", "plain")
+        email_msg["Subject"] = subject
+        email_msg["From"] = os.environ["EML_USR"]
+
+        with smtplib.SMTP(os.environ["EML_SRV"], int(os.environ["EML_PRT"])) as smtp:
+            os.environ["EML_SRV"], int(os.environ["EML_PRT"])
+            smtp.starttls()
+            smtp.login(os.environ["EML_USR"], os.environ["EML_PSS"])
+            smtp.sendmail(os.environ["EML_USR"], to, email_msg.as_string())
+            return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+def on_sms_receive(ch, method, props, body: bytes):
     # Capture received message to file
     log(body)
 
     # Parse the byte to a dict
     msg = ast.literal_eval(body.decode("UTF-8"))
-    print(time.ctime(time.time())+" : to-> "+ msg['to'] +"   msg-> "+ msg['message'])
-    log("Received : to-> "+ msg['to'] +"   msg-> "+ msg['message'])
-    if (send_sms(msg['to'], msg['message'])):
+    print(
+        time.ctime(time.time()) + " : to-> " + msg["to"] + "   msg-> " + msg["message"]
+    )
+    log("Received : to-> " + msg["to"] + "   msg-> " + msg["message"])
+    if send_sms(msg["to"], msg["message"]):
         # Acknowledge the message
         ch.basic_ack(method.delivery_tag)
         log("Message acknowledged")
@@ -47,20 +71,60 @@ def on_recieve (ch, method, props, body: bytes):
         log("Unable to send message")
 
 
+def on_email_receive(ch, method, props, body: bytes):
+    # Capture received message to file
+    log(body)
+
+    # Parse the byte to a dict
+    msg = ast.literal_eval(body.decode("UTF-8"))
+    print(
+        time.ctime(time.time())
+        + " : to-> "
+        + msg["to"]
+        + "  sbj-> "
+        + msg["subject"]
+        + "  bdy-> "
+        + msg["body"]
+    )
+    log(
+        "Received : to-> "
+        + msg["to"]
+        + "  sbj-> "
+        + msg["subject"]
+        + "  bdy-> "
+        + msg["body"]
+    )
+    if send_email(msg["to"], msg["body"], msg["subject"]):
+        # Acknowledge the message
+        ch.basic_ack(method.delivery_tag)
+        log("Email Message acknowledged")
+    else:
+        print("Unable to send email")
+        log("Unable to send email")
+
+
 try:
+    credentials = pika.PlainCredentials(
+        os.environ["RBT_MQ_USER"], os.environ["RBT_MQ_PASS"]
+    )
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(os.environ["RBT_MQ"], credentials=credentials)
+    )
+    channel = connection.channel()
+    channel.queue_declare(queue="test", durable=True)
+    channel.queue_declare(queue="email", durable=True)
+    channel.basic_consume(
+        queue="test", auto_ack=False, on_message_callback=on_sms_receive
+    )
+    channel.basic_consume(
+        queue="email", auto_ack=False, on_message_callback=on_email_receive
+    )
 
-    credentials = pika.PlainCredentials(os.environ['RBT_MQ_USER'], os.environ['RBT_MQ_PASS'])
-    connection  = pika.BlockingConnection(pika.ConnectionParameters(os.environ['RBT_MQ'], credentials = credentials))
-    channel  = connection.channel()
-    channel.queue_declare(queue = 'test', durable = True)
-
-    channel.basic_consume(queue = 'test', auto_ack = False, on_message_callback = on_recieve)
-
-    print(' [*] Waiting for messages')
+    print(" [*] Waiting for messages")
     channel.start_consuming()
 
 except Exception as e:
-    print (e)
+    print(e)
 
 except KeyboardInterrupt:
     try:
